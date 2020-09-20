c5643e37c52933e164f3e513d6549c476067e72a
Walkyst <41806921+Walkyst@users.noreply.github.com>
2020-09-20 12:40:26 +0300

    upd

--- commands/warns.js
+++ commands/warns.js
@@ -1,5 +1,5 @@
 module.exports.run = async (client, msg, args) => {
-	let memberInfo = msg.mentions.users.first() || (args[0] ? args[0] : msg.author);
+	let memberInfo = msg.mentions.users.first() || (args[0] ? {id: args[0], tag: args[0]} : msg.author);
 
 	let result = await client.userLib.promise(client.userLib.db, client.userLib.db.queryValue, 'SELECT warns FROM blacklist WHERE id = ?', [memberInfo.id]);
 
@@ -7,9 +7,9 @@ module.exports.run = async (client, msg, args) => {
 		embed = new client.userLib.discord.RichEmbed()
 			.setColor('#00FF00')
 			.setTitle(client.userLib.langf[msg.flag].warns)
-			.setDescription(client.userLib.langf[msg.flag].warnsNo.replace("{member}", memberInfo.tag ? memberInfo.tag : memberInfo))
+			.setDescription(client.userLib.langf[msg.flag].warnsNo.replace("{member}", memberInfo.tag))
 			.setTimestamp()
-			.setFooter(memberInfo.tag ? memberInfo.tag : memberInfo, memberInfo.displayAvatarURL);
+			.setFooter(memberInfo.tag, memberInfo.displayAvatarURL);
 		msg.channel.send(embed);
 		return;
 	}
@@ -19,7 +19,7 @@ module.exports.run = async (client, msg, args) => {
 		.setTitle(client.userLib.langf[msg.flag].warns)
 		.setDescription(`${client.userLib.langf[msg.flag].warnsCount} **${result.res}**.`)
 		.setTimestamp()
-		.setFooter(memberInfo.tag ? memberInfo.tag : memberInfo, memberInfo.displayAvatarURL);
+		.setFooter(memberInfo.tag, memberInfo.displayAvatarURL);
 
 	msg.channel.send(embed);
 }
