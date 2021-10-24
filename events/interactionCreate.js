6aa7f12d94d3485aff7ecdf9208ed5c123566a4f
dellyare <37771198+Dellyare@users.noreply.github.com>
2021-10-24 14:55:55 +0300

    Defer fix

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -6,7 +6,6 @@ export default async function (interaction) {
 	if (!interaction.inGuild() || !interaction.isCommand()) return;
 
 	const command = commands[interaction.commandName];
-	await interaction.deferReply({ ephemeral: interaction.options.getBoolean('ephemeral') ?? command.helpers.ephemeral });
 
 	interaction.guildSettings = await dataBase.one('SELECT * FROM nika_server WHERE id = ?', [interaction.guildId]);
 
