e1410766fb58f312f8b9822cf890c745d5923b0b
MegaVasiliy007 <megavasiliy007@vare.pw>
2021-10-31 22:40:45 +0300

    remove dbl library and add request for it

--- nika.js
+++ nika.js
@@ -1,9 +1,8 @@
 import { Client, Intents, WebhookClient } from 'discord.js';
 import log from './utils/log';
-import DBLAPI from 'dblapi.js';
 import readyEvent from './events/ready';
 import presenceController from './utils/presenceController';
-import postSDCStatistic from './utils/postSDCStatistic';
+import postStatistic from './utils/postStatistic';
 import { parentPort } from 'worker_threads';
 
 const client = new Client({
@@ -22,17 +21,11 @@ if (process.env.WEBHOOK_URL) global.discordWebhook = new WebhookClient({ url: pr
 parentPort.on('message', m => {
 	if (m === 'startPresence') {
 		presenceController();
-		postSDCStatistic();
+		postStatistic();
 		setInterval(presenceController, 30e3);
-		setInterval(postSDCStatistic, 30 * 60e3);
+		setInterval(postStatistic, 30 * 60e3);
 	}
 });
-
-if (process.env.DBL) {
-	const dbl = new DBLAPI(process.env.DBL, client);
-	dbl.on('posted', () => console.log('Server count posted!'));
-	dbl.on('error', e => console.log(`Oops! ${e}`));
-}
 
 client.once('ready', readyEvent);
 
