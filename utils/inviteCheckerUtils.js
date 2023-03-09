9d778d3d1d514da1584c0a17050933456c9fe0d1
Alexander <satan1c@ukr.net>
2023-03-09 11:43:32 +0200

    move checks to utils & reorder

---
+++ utils/inviteCheckerUtils.js
@@ -0,0 +1,63 @@
+import getInvite from './getInvite.js';
+import texts from '../config/texts.js';
+import log from './log.js';
+import { EmbedBuilder, PermissionFlagsBits } from 'discord.js';
+import colors from '../config/colors.js';
+import ignoreGuildsList from '../config/ignoreGuildsList.js';
+
+const hasInvite = async (invite, guildId) => {
+	if (!invite) return false;
+	const fetchInvite = await discordClient.fetchInvite(invite).catch(() => null);
+	return fetchInvite && fetchInvite.guild?.id !== guildId && !ignoreGuildsList.includes(fetchInvite.guild?.id);
+};
+
+const permissionsWhiteList = [
+	PermissionFlagsBits.Administrator,
+	PermissionFlagsBits.ManageRoles,
+	PermissionFlagsBits.ManageMessages,
+	PermissionFlagsBits.KickMembers,
+	PermissionFlagsBits.BanMembers,
+	PermissionFlagsBits.ViewAuditLog,
+	PermissionFlagsBits.MuteMembers,
+	PermissionFlagsBits.DeafenMembers,
+	PermissionFlagsBits.MoveMembers,
+	PermissionFlagsBits.ManageNicknames,
+	PermissionFlagsBits.ModerateMembers,
+];
+
+export async function isWhitelistedOrNoInvite(message, guildSettings) {
+	return (
+		message.channel.id === guildSettings?.channel ||
+		message.member?.permissions.any(permissionsWhiteList) ||
+		!(await hasInvite(getInvite(message.content), message.guild.id))
+	);
+}
+
+export async function tryPunish(userWarns, guildSettings, message) {
+	if (
+		(userWarns.warns > 0 && guildSettings.level === 'berserker') ||
+		(userWarns.warns > 2 && guildSettings.level === 'medium')
+	)
+		await message.member.ban(texts[guildSettings.language].banSpam).catch(() => {
+			log(`Error ban execution fail | ${message.id} | ${guildSettings.level}`);
+		});
+}
+
+export async function sendWebhook(message) {
+	if (process.env.WEBHOOK_URL) {
+		const embed = new EmbedBuilder()
+			.setAuthor({ name: 'Обнаружена попытка спама!' })
+			.setTitle('Сообщение:')
+			.setDescription(message.content)
+			.setColor(colors.red)
+			.addFields([
+				{ name: 'Нарушитель:', value: `${message.author.tag} (${message.author.id})` },
+				{ name: 'Название канала:', value: `${message.channel.name} (${message.channel.id})` },
+				{ name: 'Сервер:', value: `${message.guild.name} (${message.guild.id})` },
+			])
+			.setFooter({ text: 'Время сообщения' })
+			.setTimestamp(message.createdAt);
+
+		await discordWebhook.send({ embeds: [embed] }).catch(() => log(`${message.author.id} | Не смогла отправить лог`));
+	}
+}
--- utils/messageInviteChecker.js
+++ utils/messageInviteChecker.js
@@ -1,20 +1,11 @@
 import texts from '../config/texts.js';
-import getInvite from './getInvite';
-import { EmbedBuilder, PermissionFlagsBits } from 'discord.js';
-import colors from '../config/colors.js';
-import log from './log';
-import ignoreGuildsList from '../config/ignoreGuildsList.js';
+import { ChannelType } from 'discord.js';
 import Blacklist from '../models/blacklist.js';
 import Guild from '../models/guild.js';
-
-const hasInvite = async (invite, guildId) => {
-	if (!invite) return false;
-	const fetchInvite = await discordClient.fetchInvite(invite).catch(() => null);
-	return fetchInvite && fetchInvite.guild?.id !== guildId && !ignoreGuildsList.includes(fetchInvite.guild?.id);
-};
+import { isWhitelistedOrNoInvite, sendWebhook, tryPunish } from './inviteCheckerUtils.js';
 
 export default async function (message) {
-	if (message.author.bot || message.channel.type === 'DM') return;
+	if (message.author.bot || message.channel.type === ChannelType.DM) return;
 
 	let [userWarns, guildSettings] = await Promise.all([
 		Blacklist.findByPk(message.author.id),
@@ -25,60 +16,24 @@ export default async function (message) {
 		const language = message.guild.preferredLocale === 'ru' ? 'ru' : 'en';
 		guildSettings = { language, level: 'medium' };
 	}
-
 	if (!userWarns) userWarns = { warns: 0 };
 
-	if (userWarns.warns > 2 && (guildSettings.level === 'berserker' || guildSettings.level === 'medium'))
-		message.member.ban(texts[guildSettings.language].banSpam).catch(() => {});
+	await tryPunish(userWarns, guildSettings, message);
 
-	if (
-		message.channel.id === guildSettings?.channel ||
-		message.member?.permissions.any([
-			PermissionFlagsBits.Administrator,
-			PermissionFlagsBits.ManageRoles,
-			PermissionFlagsBits.ManageMessages,
-			PermissionFlagsBits.KickMembers,
-			PermissionFlagsBits.BanMembers,
-			PermissionFlagsBits.ViewAuditLog,
-			PermissionFlagsBits.MuteMembers,
-			PermissionFlagsBits.DeafenMembers,
-			PermissionFlagsBits.MoveMembers,
-			PermissionFlagsBits.ManageNicknames,
-		])
-	)
-		return;
-
-	if (!(await hasInvite(getInvite(message.content), message.guild.id))) return;
-
-	if (process.env.WEBHOOK_URL) {
-		const embed = new EmbedBuilder()
-			.setAuthor({ name: 'Обнаружена попытка спама!' })
-			.setTitle('Сообщение:')
-			.setDescription(message.content)
-			.setColor(colors.red)
-			.addFields([
-				{ name: 'Нарушитель:', value: `${message.author.tag} (${message.author.id})` },
-				{ name: 'Название канала:', value: `${message.channel.name} (${message.channel.id})` },
-				{ name: 'Сервер:', value: `${message.guild.name} (${message.guild.id})` },
-			])
-			.setFooter({ text: 'Время сообщения' })
-			.setTimestamp(message.createdAt);
-
-		discordWebhook.send({ embeds: [embed] }).catch(() => log(`${message.author.id} | Не смогла отправить лог`));
-	}
+	if (await isWhitelistedOrNoInvite()) return;
 
 	userWarns.warns++;
 
 	await Blacklist.upsert({ id: message.author.id, warns: userWarns.warns });
+	await sendWebhook(message);
+	await tryPunish(userWarns, guildSettings, message);
 
-	if ((userWarns.warns > 2 && guildSettings.level === 'medium') || guildSettings.level === 'berserker')
-		message.member.ban(texts[guildSettings.language].banSpam).catch(() => {});
-
-	const spammessage = await message.channel
+	const spamMessage = await message.channel
 		.send(texts[guildSettings.language].msgNoInvitePubl.replace('%author', message.author))
 		.catch(() => {});
 
-	if (spammessage) setTimeout(() => spammessage.delete().catch(() => {}), 1e4);
-	message.author.send(texts[guildSettings.language].msgNoInvite + userWarns.warns).catch(() => {});
-	message.delete().catch(() => {});
+	if (spamMessage) setTimeout(() => spamMessage.delete().catch(() => {}), 1e4);
+
+	await message.author.send(texts[guildSettings.language].msgNoInvite + userWarns.warns).catch(() => {});
+	await message.delete().catch(() => {});
 }
