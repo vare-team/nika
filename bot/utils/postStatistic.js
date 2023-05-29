a4a75a4179bfed7a7e51e13143ae8616ab12c45d
Alexander <satan1c@ukr.net>
2023-05-29 14:23:05 +0300

    update statistics posting from axios to undici

--- bot/utils/postStatistic.js
+++ bot/utils/postStatistic.js
@@ -1,26 +1,32 @@
-import { default as axios } from 'axios';
 import log from './log.js';
+import { request } from 'undici';
 
-const sdcInstance = axios.create({
-	method: 'POST',
-	baseURL: 'https://api.server-discord.com/v2/bots',
-	headers: { authorization: `SDC ${process.env.SDC}` },
-});
+const sdcInstance = async (userId, servers, shards) => {
+	await request(`https://api.server-discord.com/v2/bots/${userId}/stats`, {
+		method: 'POST',
+		headers: { authorization: `SDC ${process.env.SDC}`, 'Content-Type': 'application/json' },
+		body: JSON.stringify({ servers, shards }),
+	});
+};
 
-const topInstance = axios.create({
-	method: 'POST',
-	baseURL: 'https://top.gg/api/bots',
-	headers: { authorization: process.env.DBL },
-});
+const topInstance = async (userId, shards) => {
+	await request(`https://top.gg/api/bots/${userId}/stats`, {
+		method: 'POST',
+		headers: { authorization: process.env.DBL, 'Content-Type': 'application/json' },
+		body: JSON.stringify({ shards }),
+	});
+};
 
 export default async function () {
 	if (!process.env.SDC && !process.env.DBL) return;
 	const servers = await discordClient.shard.fetchClientValues('guilds.cache.size');
 	if (process.env.SDC)
-		sdcInstance({
-			url: `${discordClient.user.id}/stats`,
-			data: { servers: servers.reduce((p, v) => p + v, 0), shards: discordClient.shard.count },
-		});
-	if (process.env.DBL) topInstance({ url: `${discordClient.user.id}/stats`, data: { shards: servers } });
+		await sdcInstance(
+			discordClient.user.id,
+			servers.reduce((p, v) => p + v),
+			discordClient.shard.count
+		);
+
+	if (process.env.DBL) await topInstance(discordClient.user.id, servers);
 	log('{SDC} Send stats data.');
 }
